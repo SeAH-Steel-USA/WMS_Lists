8de0fc04b3bbcfea4fd38b431e04526e0a07023d
Mauricio Aramburu <maramburu98@gmail.com>
2024-08-30 17:42:01 -0500

    feat: new class to use to combine the WMS records that have the same DateTime and Millitm columns
    refactor: cleaned up weather page code

---
+++ WMS_Lists/WMS_Lists/Data/CombinedRecords.cs
@@ -0,0 +1,16 @@
+﻿namespace WMS_Lists.Data;
+public class CombinedRecords
+{
+    public DateTime? DateAndTime { get; set; }
+    public short? Millitm { get; set; }
+    public short? TagIndex0 { get; set; }
+    public double? TagIndex0_Value { get; set; }
+    public short? TagIndex1 { get; set; }
+    public double? TagIndex1_Value { get; set; }
+    public short? TagIndex2 { get; set; }
+    public double? TagIndex2_Value { get; set; }
+    public short? TagIndex3 { get; set; }
+    public double? TagIndex3_Value { get; set; }
+    public char? Status { get; set; }
+    public char? Marker { get; set; }
+}
